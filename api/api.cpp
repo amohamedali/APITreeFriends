6a48c13f8588cdd6795bcd3b8e844c55a1de21e7
cristi_a <toma.cristini@gmail.com>
2015-02-14 16:08:35 +0100

    api start

--- api/api.cpp
+++ api/api.cpp
@@ -9,6 +9,10 @@ Api::~Api() {
 
 };
 
-void Api::receiveRequest(Request *) {
-
+void Api::receiveRequest(Request *req) {
+	connecter(req); // blacklist
+	parser(req); // Jahwar
+	method(req); // Get ressource, Post tout ça
+	response(req); // Créer le header de réponse et le body
+	send(req); // Log 
 };
--- api/api.hh
+++ api/api.hh
@@ -5,13 +5,37 @@
 #include <string>
 #include <iostream>
 
+/*
+IRequest
+	-std::string 	ip
+	-short				port
+	-int					fd
+	-boolean 			state
+	-std::string 	requestBrut;
+	-IhttpReq 		req;
+	-IhttpMess 		resp;
+	-boolean 			close;
+
+IhttpReq
+	-std::string												Method;
+	-std::string												Ressource
+	-std::string 												firstline
+	-std::map<std::string, std::string> headerMap
+	-std::string 												body
+IhttpResp
+	-int											 					statusCode;
+	-std::string 												firstline;
+	-std::map<std::string, std::string> headerMap
+	-std::string 												body
+*/
+
 class Api
 {
-	Connection 					connecter; // Log, blacklist
+	Connection 					connecter; // ex :post mais je suis sur on peut faire d truck nul avan blacklist, SSL
 	RequestParser 			parser; // Jahwar
 	MethodAction 				method; // Get ressource, Post tout ça
-	ResponseBuilder			response; // Créer le header de réponse et le body
-	Sender							send; // Aprés création de la réponse, encoding, close peut etre, tout ça
+	ResponseBuilder			response; // compression, Créer le header de réponse et le body
+	Sender							send; // Aprés création de la réponse, encoding, close peut etre, tout ça,Log 
 
 public:
 	Api();
