d2e0985adc4de8b0933a0b2c80f33c7dce04ccb0
keysim <keysim@hotmail.fr>
2015-02-16 12:26:50 +0100

    Update IRunable.hh
    Commentaires

--- api/IRunable.hh
+++ api/IRunable.hh
@@ -1,16 +1,41 @@
 #ifndef IRUNABLE_HH_
 #define IRUNABLE_HH_
 
+/*!
+* \file IRunable.hh
+* \brief Interface d'exécutiondes des modules
+* \author menard_s
+* \version 0.34
+*/
+
 #include "IConnection.hh"
 
+/*! \namespace APITreeFriends
+*
+* espace de nommage regroupant la totalité des classes
+* de l'api
+*/
 namespace APITreeFriends
 {
+  /*! \class IRunable
+  *   \brief Classe représentant la gestion d'exécution par priorité des modules
+  */
   class IRunable
     {
     public:
+    /*!
+    * \brief Constructeur virtuel
+    */
       virtual ~IRunable() {}
-      
+      /*!
+      * \brief Getter de la priorité du module associé
+      * \return int, Retourne un nombre entre 1 et 100 représentant la priorité
+      */
       virtual int   getPriority() const = 0;
+      /*!
+      * \brief Exécution du module associé à la requête
+      * \param connection IConnection & Variable contenant les informations d'une requête
+      */
       virtual void  run(IConnection &) = 0;
     };  
 };
