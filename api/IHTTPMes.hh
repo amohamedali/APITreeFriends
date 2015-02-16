1de9687dcb12ff2253097902ab656bea11434570
cristi_a <toma.cristini@gmail.com>
2015-02-16 11:50:55 +0100

    proto dox

--- api/IHTTPMes.hh
+++ api/IHTTPMes.hh
@@ -13,7 +13,7 @@
 
 /*! \namespace APITreeFriends
 *   
-* espace de nommage regroupant la totalité des classes
+* Espace de nommage regroupant la totalité des classes
 * de l'api
 */
 namespace APITreeFriends
@@ -26,20 +26,45 @@ namespace APITreeFriends
     typedef std::map<std::string, std::string> headerMap;
     public:
       /*!
-       * \brief Constructeur virtuel
+       * \brief Destructeur virtuel
        */
       virtual ~IHTTPMes() {}
       /*!
        * \brief Getter de la map de headerField
+       * \return std::map<std::string, std::string> headerMap
        */
       virtual headerMap           &getHeader() const = 0;
+      /*!
+       * \brief Getter du content du message
+       * \return std::string body
+       */
       virtual std::string         &getBody() const = 0;
+      /*!
+       * \brief Getter de la HTTPVersion
+       * \return std::string HTTPVersion
+       */
       virtual std::string         &getHTTPVersion() const = 0;
-
+      /*!
+       * \brief Setter de la map de headerField
+       * \param headerMap Map de headerfield de type std::map<std::string, std::string>
+       */
       virtual void                setHeader(const headerMap &) = 0;
+      /*!
+       * \brief Setter du content du message
+       * \param Content Body du message de type std::string
+       */
       virtual void                setBody(const std::string &) = 0;
+      /*!
+       * \brief Setter du la HTTPVersion
+       * \param HTTPVersion Version HTTP du message de type std::string
+       */
       virtual void                setHTTPVersion(const std::string &) = 0;
 
+      /*!
+       * \brief Getter d'un headerField value
+       * \param headerKey Nom du header de type std::string
+       * \return std::string value d'un HeaderField
+       */
       virtual std::string         &getHeaderField(const std::string &) const = 0;
       virtual void                setHeaderField(const std::string &, const std::string &) = 0;
       virtual void                removeHeaderField(const std::string &) = 0;
