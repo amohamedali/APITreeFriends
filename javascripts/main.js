7a8630b26677b29c352fb43612598a0d079e0588
MIdo <ahmed.mohamedali@epitech.eu>
2015-02-16 14:16:50 +0100

    fix links scroll

--- javascripts/main.js
+++ javascripts/main.js
@@ -6,3 +6,19 @@ $('#header_wrap a.btn').on('click', function(e){
     }, 1000);
 });
 
+$('#navigation-bar a').on('click', function(e){
+    e.preventDefault();
+    var target = $($(this).attr('href'));
+    $('html, body').stop().animate({
+       scrollTop: target.offset().top
+    }, 1000);
+});
+
+$("#navbar-brand").on('click', function(e){
+    e.preventDefault();
+    var target = $($(this).attr('href'));
+    $('html, body').stop().animate({
+       scrollTop: 0
+    }, 1000);
+});
+
